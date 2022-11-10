799febffdad11a88feb12dc08509265041439830
Андрей Голишевский <a@vg9.ru>
2022-11-10 23:03:02 +0300

    correct errors

--- yatube/core/tests/test_urls.py
+++ yatube/core/tests/test_urls.py
@@ -1,19 +1,14 @@
-from django.core.cache import cache
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.urls import reverse
 
 
 class CoreURLsTests(TestCase):
-    def setUp(self):
-        self.guest_client = Client()
-        cache.clear()
-
     def test_urls_uses_correct_template(self):
         """URL-адрес использует соответствующий шаблон."""
         templates_url_names = {
-            reverse('not_exists'): 'core/404.html',
+            reverse('/not_exists/'): 'core/404.html',
         }
         for address, template in templates_url_names.items():
             with self.subTest(address=address):
-                response = self.guest_client.get(address)
+                response = self.client.get(address)
                 self.assertTemplateUsed(response, template)
